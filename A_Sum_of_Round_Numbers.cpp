907e159aceda6733dcec2f186e461b74aa143145
Dharun <dharunkaarthick07@gmail.com>
2025-12-08 23:41:38 +0530

    New problems

---
+++ A_Sum_of_Round_Numbers.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		vector<int> ans;
+		int p = 1;
+		while (n > 0) {
+			if (n % 10 > 0) {
+				ans.push_back((n % 10) * p);
+			}
+			n /= 10;
+			p *= 10;
+		}
+		cout << ans.size() << endl;
+		for (auto num : ans) cout << num << " ";
+		cout << endl;
+	}
+}
--- D_Wishing_Cards.cpp
+++ D_Wishing_Cards.cpp
@@ -1,41 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+const ll N = -4e18;
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int tt;
+    int tt; 
     cin >> tt;
     while(tt--){
-        int n, k;
-        cin >> n >> k;
+        int n, K;
+        cin >> n >> K;
         vector<int> a(n);
         for(int i=0;i<n;i++) cin >> a[i];
 
-        ll ans = 0;
+        vector<vector<ll>> dp(K+1, vector<ll>(K+1, N));
+        dp[0][0] = 0;
 
-        for(int M=1; M<=k; M++){
-            int p = 0, h = 1;
-            vector<int> pos;
-            while(h <= M){
-                while(p < n && a[p] < h) p++;
-                if(p == n) goto nextM;
-                pos.push_back(p);
-                p++;
-                h++;
+        for(int i=0;i<n;i++){
+            int c = min(a[i], K);
+            vector<vector<ll>> ndp(K+1, vector<ll>(K+1, N));
+
+            for(int m=0;m<=K;m++){
+                for(int u=0;u<=K;u++){
+                    if(dp[m][u] == N) continue;
+
+                    for(int x=0;x<=c && u+x<=K;x++){
+                        int nm = max(m,x);
+                        int nu = u+x;
+                        ll v = dp[m][u] + nm;
+                        ll &r = ndp[nm][nu];
+                        if(v > r) r = v;
+                    }
+                }
             }
-            {
-                ll cur = 0;
-                int last = pos.back();
-                for(int i=0;i<pos.size();i++) cur += (i+1);
-                cur += 1LL * (n - last - 1) * M;
-                ans = max(ans, cur);
-            }
-            nextM:;
+            dp.swap(ndp);
         }
 
-        cout << ans << '\n';
+        ll ans = 0;
+        for(int m=0;m<=K;m++)
+            for(int u=0;u<=K;u++)
+                ans = max(ans, dp[m][u]);
+
+        cout << ans << "\n";
     }
 }
