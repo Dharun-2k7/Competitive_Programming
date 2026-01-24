9838aaba644b6ecdedf5fa9626b4af6e2e1cd6e6
Dharun <dharunkaarthick07@gmail.com>
2026-01-24 19:10:04 +0530

    ABC 442

---
+++ C_Peer_Review.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define end "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+
+void solve(){
+    int n, m; 
+    cin >> n >> m;
+    vi deg(n + 1, 0);
+
+    while(m--){
+        int a, b;  cin >> a >> b;
+        deg[a]++;
+        deg[b]++;
+    }
+
+    rep(i,1,n+1){
+        int k = n - 1 - deg[i];
+        cout << (k >= 3 ? (k * (k - 1) * (k - 2)) / 6 : 0);
+        cout << (i == n ? '\n' : ' ');
+    }
+}
+int32_t main(){
+    fast
+    //int t;
+    //cin >> t;
+    //while(t--){
+        solve();
+    //}
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
