d36a2ba3479c60c6f432caf6e71dada51ffff2e9
Dharun <dharunkaarthick07@gmail.com>
2026-02-06 22:44:19 +0530

    new commit

---
+++ C_Contrast_Value.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define end "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve(){
+    int n;
+    cin >> n;
+    vi a(n);
+    rep(i,0,n) cin >> a[i];
+
+    vi b;
+    for(int i = 0; i < n; i++){
+        if(b.empty() || b.back() != a[i])
+            b.pb(a[i]);
+    }
+
+    int m = b.size();
+    if(m == 1){
+        cout << 1 << end;
+        return;
+    }
+
+    int ans = 2;
+    for(int i = 1; i < m-1; i++){
+        int x = b[i] - b[i-1];
+        int y = b[i+1] - b[i];
+        if(x * y < 0)
+            ans++;
+    }
+
+    cout << ans << end;
+}
+
+int32_t main(){
+    fast
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
