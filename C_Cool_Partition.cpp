63020d9ec70d110ecee8eaa79ca88d623a9399bc
Dharun <dharunkaarthick07@gmail.com>
2026-02-13 21:01:31 +0530

    new problems

---
+++ C_Cool_Partition.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve(){
+    int n; cin >> n;
+    vi v(n);
+    vi dis(n), frq(n+1);
+    rep(i,0,n){
+        cin >> v[i];
+        frq[v[i]]++;
+        if(frq[v[i]] == 1) dis[i] = 1;
+        dis[i] += (i ? dis[i-1] : 0);
+    }
+
+    fill(all(frq), 0);
+
+    int ans = 0;
+    int r = n-1;
+    int tot = 0;
+
+    for(int i = n-1; i >= 0; i--){
+        frq[v[i]]++;
+        if(frq[v[i]] == 1) tot++;
+
+        if(tot == dis[r]){
+            ans++;
+            for(int j = i; j <= r; j++) frq[v[j]] = 0;
+            r = i-1;
+            tot = 0;
+        }
+    }
+
+    cout << ans << nl;
+}
+
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
