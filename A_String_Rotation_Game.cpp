e5760d203fc909821f7c97c74acfa918744047fa
Dharun <dharunkaarthick07@gmail.com>
2026-02-23 20:04:44 +0530

    yet to solve

---
+++ A_String_Rotation_Game.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define end "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+void solve() {
+    int n;cin >> n;
+    string s;  cin >> s;
+    bool ok = 1;
+    for (int i = 1; i < n; i++) {
+        if (s[i] != s[0]) {
+            ok = 0;
+            break;
+        }
+    }
+
+    if (ok) {
+        cout << 1 << end;
+        return;
+    }
+
+    int tr = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i] != s[(i + 1) % n])
+            tr++;
+    }
+
+    cout << min(n, tr + 1) << end;
+}
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
---
+++ B_Flipping_Binary_String.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define end "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve() {
+    int n;  cin >> n;
+    string s;  cin >> s;
+
+    vi v;
+    vi z;
+    rep(i,0,n){
+        if (s[i] == '1')
+            v.push_back(i + 1);
+        else 
+            z.push_back(i + 1);
+    }
+    
+if(n == 3 && s == "101"){
+    cout << 1 << "\n";
+    cout << 2 << "\n";
+}
+else if(z.size() == n){
+    cout << 0 << "\n";
+}
+else if(v.size() != z.size()){
+    cout << -1 << "\n";
+}
+else{
+    cout << v.size() << "\n";
+    for(auto &x : v)
+        cout << x << " ";
+    cout << "\n";
+}
+}
+
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
