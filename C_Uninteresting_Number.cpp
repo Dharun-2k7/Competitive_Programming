31d13a85b386e49ac29f90477d4c50a1c2ef35bd
Dharun <dharunkaarthick07@gmail.com>
2026-03-12 11:46:13 +0530

    Number theory practice

---
+++ C_Uninteresting_Number.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+void solve(){
+    string s; 
+    cin >> s;
+
+    int sm = accumulate(all(s), 0LL) - '0' * s.size();
+    int tw = count(all(s),'2');
+    int th = count(all(s),'3');
+
+    rep(i,0,min(10LL,tw+1)){
+        rep(j,0,min(10LL,th+1)){
+            if((sm + i*2 + j*6) % 9 == 0){
+                cout<<"YES"<<nl;
+                return;
+            }
+        }
+    }
+
+    cout<<"NO"<<nl;
+}
+
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
