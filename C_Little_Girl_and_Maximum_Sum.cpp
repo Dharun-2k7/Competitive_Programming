8cfd998575274168a698af0181b24644e779cab4
Dharun <dharunkaarthick07@gmail.com>
2026-03-27 01:25:01 +0530

    new problem

---
+++ C_Little_Girl_and_Maximum_Sum.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve(){
+    int n; cin >>n;
+    int q; cin >>q;
+    vi a(n+1);
+    rep(i,1,n+1) cin >>a[i];
+    sort(all(a),greater<int>());
+    
+    int l[n]={0}, r[n]={0},freq[n]={0};
+    int ans=0;
+    rep(i,0,q){
+        int x,y; cin >>x >>y;
+        l[x-1]++;
+        r[y-1]++;
+    }
+    int c=0;
+    rep(i,0,n){
+        c+=l[i];
+        freq[i]+=c;
+        c-=r[i];
+    }
+    sort(freq,freq+n,greater<int>());
+    rep(i,0,n) ans+=freq[i]*a[i];
+    cout <<ans <<nl;
+        
+}
+    
+
+
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    //test();
+    solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
