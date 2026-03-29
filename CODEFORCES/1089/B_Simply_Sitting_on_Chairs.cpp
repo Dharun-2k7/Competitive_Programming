9e3c78754c35a9d0029acbf66ad0006b9f64e85a
Dharun <dharunkaarthick07@gmail.com>
2026-03-30 01:34:33 +0530

    Codeforces Round 1089

---
+++ CODEFORCES/1089/B_Simply_Sitting_on_Chairs.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve(){
+    int n; cin >> n;
+    vi p(n + 1),diff(n + 2, 0);
+    rep(i,1,n+1) cin >> p[i];
+    rep(j,1,n+1){
+        if(p[j] <= j){
+            if(j + 1 <= n){
+                diff[j + 1]++;
+                diff[n + 1]--;
+            }
+        }
+        else{
+            if(j + 1 <= p[j] - 1){
+                diff[j + 1]++;
+                diff[p[j]]--;
+            }
+        }
+    }
+    int cur = 0, ans = 0;
+    rep(l,1,n+1){
+        cur += diff[l];
+        ans = max(ans, cur + 1);
+    }
+
+    cout << ans << nl;
+}
+
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
---
+++ CODEFORCES/1089/C_1_A_Simple_GCD_Problem_Easy_Version.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve(){
+    int n; cin >> n;
+    vi a(n), b(n);
+    rep(i,0,n) cin >> a[i];
+    rep(i,0,n) cin >> b[i];
+
+    int ans = 0;
+
+    if(n == 1){
+        cout << 0 << nl;
+        return;
+    }
+
+    auto lcm = [&](int x, int y){
+        return (x / gcd(x, y)) * y;
+    };
+
+    rep(i,0,n){
+        if(i == 0){
+            int g = gcd(a[i], a[i+1]);
+            if(g < a[i]) ans++;
+        }
+        else if(i == n-1){
+            int g = gcd(a[i-1], a[i]);
+            if(g < a[i]) ans++;
+        }
+        else{
+            int x = gcd(a[i-1], a[i]);
+            int y = gcd(a[i], a[i+1]);
+            int nd = lcm(x, y);
+            if(nd < a[i]) ans++;
+        }
+    }
+    cout << ans << nl;
+}
+
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
