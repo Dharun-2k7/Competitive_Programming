a8552a159b2965af1c97e19835dc5dacbb1bbac1
Dharun <dharunkaarthick07@gmail.com>
2026-03-31 23:45:18 +0530

    new problems

---
+++ D_Satyam_and_Counting.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve(){
+    int n; cin >> n;
+    vi bot(n + 5, 0), top(n + 5, 0);
+
+    rep(i,0,n){
+        int x, y; cin >> x >> y;
+        if(y == 0) bot[x] = 1;
+        else top[x] = 1;
+    }
+
+    int cnt0 = 0, cnt1 = 0;
+    rep(i,0,n+5){
+        cnt0 += bot[i];
+        cnt1 += top[i];
+    }
+    int ans = 0;
+    rep(x,0,n+5){
+        if(bot[x] && top[x]){
+            ans += (cnt0 - 1) + (cnt1 - 1);
+        }
+        if(x - 1 >= 0 && x + 1 <= n){
+            if(top[x] && bot[x-1] && bot[x+1]) ans++;
+            if(bot[x] && top[x-1] && top[x+1]) ans++;
+        }
+    }
+    cout << ans << nl;
+}
+
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
