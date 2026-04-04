c55562a509c06e50dff8deb788bd59e8cd35c0db
Dharun <dharunkaarthick07@gmail.com>
2026-04-05 01:09:30 +0530

    Codeforces Round 1090(Div 4)

---
+++ CODEFORCES/1090/E_The_67_th_XOR_Problem.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve(){
+    int n;cin >>n;
+    vi a(n);
+    rep(i,0,n) cin >>a[i];
+    vi mask(31, 0);
+
+    // build mask (xor basis)
+    for(int x : a){
+        revrep(b,30,0){
+            if(!(x & (1LL << b))) continue;
+
+            if(!mask[b]){
+                mask[b] = x;
+                break;
+            }
+            x ^= mask[b];
+        }
+    }
+
+    // maximize answer
+    int ans = 0;
+    revrep(b,30,0){
+        ans = max(ans, ans ^ mask[b]);
+    }
+
+    cout << ans << nl;
+}
+
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
