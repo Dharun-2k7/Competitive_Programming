fb53c03dfc7ae55fb2ef1d83c598dcf09c1833a9
Dharun <dharunkaarthick07@gmail.com>
2026-04-08 20:44:56 +0530

    upsolve B

---
+++ CODEFORCES/1091/B_Flip_the_Bit_Easy_Version.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve(){
+    int n, k; 
+    cin >> n >> k;
+    vi a(n);
+    for(auto &i : a) cin >> i;
+    vi p(k);
+    for(auto &i : p) cin >> i;
+    int val1 = a[p[0]-1];
+    int val2 = val1;
+    int ct1 = 0, ct2 = 0;
+    rep(i, 0, p[0]){
+        if(a[i] != val1){
+            ct1++;
+            val1 ^= 1;
+        }
+    }
+    revrep(i, n-1, p[0]-1){
+        if(a[i] != val2){
+            ct2++;
+            val2 ^= 1;
+        }
+    }
+    cout << max(ct1, ct2) << nl;
+}
+
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
