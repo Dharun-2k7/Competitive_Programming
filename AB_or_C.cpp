2a378cd00d500839611f9baefd2b8b0e8b9f03f3
Dharun <dharunkaarthick07@gmail.com>
2026-04-09 23:49:21 +0530

    new problem

---
+++ AB_or_C.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve() {
+    int n;  cin >> n;
+    vpii v;
+    v.reserve(3*n);
+    rep(i,0,n){
+        int a,b,c; cin >> a >> b >> c;
+        v.pb({a,i});
+        v.pb({b,i});
+        v.pb({c,i});
+    }
+    sort(all(v));
+    vi cnt(n, 0);
+    int cov = 0;
+    int ans = LLONG_MAX;
+    int l = 0;
+    for(int r = 0; r < 3*n; r++) {
+        if(cnt[v[r].second]++ == 0) cov++;
+        while(cov == n) {
+            ans = min(ans, v[r].first - v[l].first);
+            if(--cnt[v[l].second] == 0) cov--;
+            l++;
+        }
+    }
+
+    cout << ans << nl;
+}
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
---
+++ A_Alice_s_Adventures_in_Chess.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve(){
+    int n, a, b; cin >> n >> a >> b;
+    string s;  cin >> s;
+    int xn = 0, yn = 0;
+    rep(i,0,n){
+        char c = s[i];
+        xn += (c == 'E');
+        xn -= (c == 'W');
+        yn += (c == 'N');
+        yn -= (c == 'S');
+    }
+    int x = 0, y = 0;
+    rep(i,0,n){
+        char c = s[i];
+        x += (c == 'E');
+        x -= (c == 'W');
+        y += (c == 'N');
+        y -= (c == 'S');
+        rep(q,0,30){
+            if(x + q * xn == a && y + q * yn == b){
+                cout << "YES" << nl;
+                return;
+            }
+        }
+    }
+    cout << "NO" << nl;
+}
+
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
