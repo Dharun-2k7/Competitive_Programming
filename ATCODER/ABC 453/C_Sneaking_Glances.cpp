63bef252d2a5875bb27eacf3f965e1ce7b892021
Dharun <dharunkaarthick07@gmail.com>
2026-04-11 18:42:19 +0530

    ABC 453

---
+++ ATCODER/ABC 453/C_Sneaking_Glances.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve(){
+    int n; cin >> n;
+    vi a(n);
+    rep(i,0,n) cin >> a[i];
+    int ans = 0;
+    for(int mask = 0; mask < (1 << n); mask++){
+        int cur = 1; 
+        int cnt = 0;
+        rep(i,0,n){
+            int prev = cur;
+            if(mask & (1 << i)){
+                cur += 2 * a[i];
+            }else{
+                cur -= 2 * a[i];
+            }
+            if(prev > 0 && cur < 0) cnt++;
+            if(prev < 0 && cur > 0) cnt++;
+        }
+
+        ans = max(ans, cnt);
+    }
+
+    cout << ans << nl;
+}
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    //test();
+    solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
