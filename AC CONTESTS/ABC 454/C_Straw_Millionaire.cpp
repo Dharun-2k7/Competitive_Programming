0ce0d059dd1b3d1eec0fbec3747359580641e4e0
Dharun <dharunkaarthick07@gmail.com>
2026-04-18 19:16:37 +0530

    AtCoder Beginner Contest 454

---
+++ AC CONTESTS/ABC 454/C_Straw_Millionaire.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve(){
+    int n,m; cin >>n>>m;
+    vvi g(n+1);
+    rep(i,0,m){
+        int a,b;cin >>a>>b;
+        g[a].pb(b);
+    }
+    vi vis(n+1,0);
+    queue<int>q;
+
+    q.push(1);
+    vis[1]=1;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (auto v : g[u]) {
+            if (!vis[v]) {
+                vis[v] = 1;
+                q.push(v);
+            }
+        }
+    }
+    int ans = 0;
+    rep(i,1,n+1) ans += vis[i];
+    cout << ans << nl;
+}
+
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    //test();
+    solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
---
+++ AC CONTESTS/ABC 454/D_xx.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+string norm(string s){
+    string t="";
+    for(auto c:s){
+        t.pb(c);
+        while(t.size()>=4){
+            int n=t.size();
+            if(t[n-4]=='(' && t[n-3]=='x' && t[n-2]=='x' && t[n-1]==')'){
+                rep(i,0,4) t.pop_back();
+                t.pb('x');
+                t.pb('x');
+            }
+            else break;
+        }
+    }
+    return t;
+}
+
+void solve(){
+    string a,b;
+    cin >> a >> b;
+    (norm(a)==norm(b)) ? cout<<"Yes"<<nl : cout<<"No"<<nl;
+}
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
