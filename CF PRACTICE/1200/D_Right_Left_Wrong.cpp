4cd385e3720efec220086f8fad07f313a8bc20a3
Dharun <dharunkaarthick07@gmail.com>
2026-05-17 17:48:37 +0530

    Good beginner friendly problem to learn prefix sum and two pointer

---
+++ CF PRACTICE/1200/D_Right_Left_Wrong.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve(){
+   int n; cin >>n;
+   vi a(n);
+   rep(i,0,n) cin >>a[i];
+   string s;cin >>s;
+   vi pref(n+1,0);
+   rep(i,0,n) pref[i+1]=pref[i]+a[i];
+   int l=0;
+   int r=n-1;
+   int ans=0;
+   while(l<r){
+    while(l<n && s[l]!='L') l++;
+    while(r>=0 && s[r]!='R') r--;
+    if(l<r){
+        ans+=pref[r+1]-pref[l];
+        l++;
+        r--;
+    }
+   } 
+   cout<<ans<<nl;
+   
+}
+
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
