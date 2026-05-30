da0f2dc3dbda2dbe6f6af66797b57acaba7ca4c2
Dharun <dharunkaarthick07@gmail.com>
2026-05-30 22:53:23 +0530

    Codeforces Round 1101 (Div. 2)

---
+++ CF CONTESTS/1101/C_1_Seating_Arrangement_Easy_Version.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve(){
+    int n,x,s;cin>>n>>x>>s;
+    string u; cin >> u;
+    vi dp(x + 1, -1);
+    dp[x] = 0;
+    for(char c : u){
+        vi ndp = dp;
+        rep(j,0,x + 1){
+            if(dp[j] == -1) continue;
+            int k = dp[j];
+            int ps = (x - j) * s - k;
+            if((c == 'I' || c == 'A') && j > 0){
+                ndp[j - 1] = max(ndp[j - 1], k + 1);
+            }
+            if((c == 'E' || c == 'A') && ps > 0){
+                ndp[j] = max(ndp[j], k + 1);
+            }
+        }
+        dp = ndp;
+    }
+
+    int ans = 0;
+    rep(j,0,x + 1) ans = max(ans, dp[j]);
+    cout << ans << nl;
+}
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
