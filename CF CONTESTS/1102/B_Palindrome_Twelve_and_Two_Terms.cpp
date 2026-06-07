4d239992b197a9a09a78693f3c4097f1f80d0aaf
Dharun <dharunkaarthick07@gmail.com>
2026-06-07 23:00:27 +0530

    Codeforces Round 1102 (Div. 2)

---
+++ CF CONTESTS/1102/B_Palindrome_Twelve_and_Two_Terms.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve(){
+    int n;cin >>n;
+    int r=n%12;
+    int a;
+    if(r==10){
+        if(n==10){
+            cout<<-1<<nl;
+            return;
+        }
+        a=22;
+    }
+    else if(r==11) a=11;
+    else a=r;
+    cout<<a<<" "<<(n-a)<<nl;
+
+
+}
+
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+   
+ given constrainst : -
+     
+     a+b=n
+ 
+    The number a is a palindrome.∗
+    The number b is divisible by 12
+
+    so if our given n is <12 return  return n+0 if n=10 
+          return -1 
+
+
+          our b must be a multiple of 12 , 
+.
+
+
+*/
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
---
+++ CF CONTESTS/1102/C_Vessels_Heights_and_Two_Versions_Easy_Version.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pll pair<long long,long long>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define revrep(i,a,b) for(int i=a;i>=b;i--)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+void solve(){
+    int n; cin >>n;
+    vi a(n);
+    rep(i,0,n) cin >>a[i];
+    vi ans(n);
+    rep(i,0,n){
+        vi w(n,0);
+        
+        int mx = 0; 
+        rep(st,1,n){
+            int cur=(i+st)%n;
+            int prv=(i+st-1)%n;
+            mx=max(mx,a[prv]); 
+            w[cur]=mx; 
+        }
+        mx = 0; 
+        rep(st,1,n){
+            int cur=(i-st+n)%n;
+            int prv=(i-st+1+n)%n;
+            mx=max(mx,a[cur]); 
+            w[cur]=min(w[cur],mx); 
+        }
+        
+        int sum=0;
+        rep(j,0,n)sum+=w[j]; 
+        ans[i]=sum;
+    }
+    rep(i,0,n) cout<<ans[i]<<" ";
+    cout<<nl;
+}
+void test(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
+
+int32_t main(){
+    fast
+    test();
+    //solve();
+    return 0;
+}
+
+/*
+ ██████████   █████                                              
+░░███░░░░███ ░░███                                               
+ ░███   ░░███ ░███████    ██████   ████████  █████ ████ ████████ 
+ ░███    ░███ ░███░░███  ░░░░░███ ░░███░░███░░███ ░███ ░░███░░███
+ ░███    ░███ ░███ ░███   ███████  ░███ ░░░  ░███ ░███  ░███ ░███
+ ░███    ███  ░███ ░███  ███░░███  ░███      ░███ ░███  ░███ ░███
+ ██████████   ████ █████░░████████ █████     ░░████████ ████ █████
+░░░░░░░░░░   ░░░░ ░░░░░  ░░░░░░░░ ░░░░░       ░░░░░░░░ ░░░░ ░░░░░
+
+ Author: Dharun
+*/
